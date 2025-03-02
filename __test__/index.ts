16add01da7f31c3fe4cc7aef2bca365aacb561fc
Tyowk <tyowk.personal@gmail.com>
2025-03-02 06:07:49 +0000

    test dir

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,14 +1,14 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const dist_1 = require('../dist');
-const client = new dist_1.ShouwClient({
+import { ShouwClient } from '../dist';
+
+const client = new ShouwClient({
     token: process.env.TOKEN,
     prefix: 'nou',
     intents: ['Guilds', 'GuildMessages', 'MessageContent'],
     events: ['messageCreate']
 });
+
 client.command({
     name: 'meow',
     type: 'messageCreate',
-    code: `$description[meow]`
+    code: '$description[meow]'
 });
